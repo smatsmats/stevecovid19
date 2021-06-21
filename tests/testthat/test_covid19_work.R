07de2c8e36fccae737f329072dfa8b8f0161ab21
Stephen Mats Mats <willey@washington.edu>
2021-06-21 08:58:04 -0700

    is this how we use renv?

--- tests/testthat/test_covid19_work.R
+++ tests/testthat/test_covid19_work.R
@@ -1,25 +1,5 @@
-library(testthat)
-
-# for sure these
-library("aws.s3")
-library("zoo")
-library("scales")
-
-#library("tidyverse")   # maybe we don't need the whole -verse
-# todyverse things
-library("dplyr")
-library("lubridate")
-library("tidyr")
-library("ggplot2")
-library("stringr")
-
-#for testing
-library("testthat")
-
-# mapping things
-library("ggmap")
-library("maps")
-library("mapdata")
+library(renv)
+renv::restore()
 
 
 # test data
