b62b59a719f55d1e05295a89a2221bc489d3767e
Stephen Mats Mats <willey@washington.edu>
2021-06-22 08:18:45 -0700

    try a renv::restore()

--- covid19_install_packages.R
+++ covid19_install_packages.R
@@ -2,23 +2,26 @@ r = getOption("repos")
 r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
 
+install.packages("renv")
+renv::restore()
+
 # for sure these
-install.packages("aws.s3")
-install.packages("zoo")
-install.packages("scales")
-
-#install.packages("tidyverse")   # maybe we don't need the whole -verse
-# todyverse things
-install.packages("dplyr")
-install.packages("lubridate")
-install.packages("tidyr")
-install.packages("ggplot2")
-install.packages("stringr")
-
-#for testing
-install.packages("testthat")
-
-# mapping things
-install.packages("ggmap")
-install.packages("maps")
-install.packages("mapdata")
+#install.packages("aws.s3")
+#install.packages("zoo")
+#install.packages("scales")
+#
+##install.packages("tidyverse")   # maybe we don't need the whole -verse
+## todyverse things
+#install.packages("dplyr")
+#install.packages("lubridate")
+#install.packages("tidyr")
+#install.packages("ggplot2")
+#install.packages("stringr")
+#
+##for testing
+#install.packages("testthat")
+#
+## mapping things
+#install.packages("ggmap")
+#install.packages("maps")
+#install.packages("mapdata")
